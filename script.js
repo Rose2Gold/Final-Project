5bf2f99f94bfce9b87ab2583bf9ee0fc871d196b
Rose2Gold <sharonadedokun09@gmail.com>
2025-08-12 11:27:53 -0400

    Small edits

--- script.js
+++ script.js
@@ -8,29 +8,3 @@ function modeToggle() {
    var nav = document.getElementById("nav")
    nav.classList.toggle("dark-Nav");
 }
-
-// function toggleMode(){
-//     let button = document.getElementById("mode");
-//     if (button.textContent == "Dark Mode") {
-//       darkMode();
-//     }
-//     else {
-//       lightMode() ; 
-//     }
-// }
-
-// function lightMode(){
-//     document.body.style.backgroundColor = "#E9FFF9";
-//     document.body.style.color = "#7180AC";
-//     let button= document.getElementById("mode");
-//     button.textContent ="Light Mode";
-    
-// }
-
-// function darkMode(){
-//     document.body.style.backgroundColor = "#7180AC";
-//     document.body.style.color ="#E9FFF9";
-//     let button= document.getElementById("mode");
-//     button.textContent ="Dark Mode";
-    
-// }
